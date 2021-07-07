38976bc362207be6e90682e134df762d5498d3c9
s.gulmirzoev <s.gulmirzoev@guarantpay.com>
2021-07-07 18:54:41 +0300

    #GP-2214 modify FeePayOptions model

--- src/models/FeePayOptions.php
+++ src/models/FeePayOptions.php
@@ -21,19 +21,6 @@ class FeePayOptions
     private $description;
 
     /**
-     * DealTypes constructor.
-     * @param $id
-     * @param $name
-     * @param $description
-     */
-    public function __construct($id, $name, $description)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->description = $description;
-    }
-
-    /**
      * @return mixed
      */
     public function getId()
