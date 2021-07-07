5b0661618c5da210ecc2baeb743cb71ac1f2cb23
s.gulmirzoev <s.gulmirzoev@guarantpay.com>
2021-07-07 18:54:43 +0300

    #GP-2214 add PaymentMethod model

---
+++ src/models/PaymentMethod.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Guarantpay\models;
+/**
+ * Class PaymentMethod
+ */
+class PaymentMethod
+{
+    /**
+     * @var
+     */
+    private $id;
+    /**
+     * @var
+     */
+    private $name;
+
+    /**
+     * @var
+     */
+    private $type;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
